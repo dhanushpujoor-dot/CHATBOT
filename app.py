10b1d2bc112a3db58bf4b9bbb4266084f83bebb2
dhanushpujoor-dot <dhanushpujoor17@gmail.com>
2025-09-15 09:59:48 +0530

    updated UI and added TTS

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
 from rag_chain import get_chain
+from langchain_core.output_parsers import StrOutputParser
+import os  #for macOS TTS
 
 st.set_page_config(page_title="Chatbot", page_icon="🤖", layout="centered")
 st.title("🤖 RAG Chatbot")
@@ -11,6 +13,14 @@ if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
     st.session_state.chain = get_chain(st.session_state.chat_history)
 
+# --- Slidebar Controls ---
+on = st.sidebar.toggle("Text-To-Speech")
+if on:
+    st.sidebar.write("TTS Enabled")
+
+if st.sidebar.button('CLEAR 🗑'):
+    st.session_state.chat_history.clear()
+
 # --- Display Previous Messages ---
 for msg in st.session_state.chat_history:
     if isinstance(msg, HumanMessage):
@@ -19,6 +29,8 @@ for msg in st.session_state.chat_history:
     elif isinstance(msg, AIMessage):
         with st.chat_message("assistant"):
             st.write(msg.content)
+
+parser = StrOutputParser() # Ensure outputs are strings
 
 user_input = st.chat_input("Type your message here...")
 
@@ -33,3 +45,5 @@ if user_input:
     st.session_state.chat_history.append(AIMessage(content=output))
     with st.chat_message("assistant"):
         st.write(output)
+        if on:
+            os.system(f'say "{output}"')
