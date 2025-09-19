7bad17840c612cb1fa6defc62606474279ab9e1e
dhanushpujoor-dot <dhanushpujoor17@gmail.com>
2025-09-19 10:46:08 +0530

    updated UI and added STT

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from langchain_core.messages import HumanMessage, AIMessage
 from rag_chain import get_chain
 from langchain_core.output_parsers import StrOutputParser
 import os  #for macOS TTS
+import speech_recognition as sr
+
+# Initialize recognizer
+recognizer = sr.Recognizer()
 
 st.set_page_config(page_title="Chatbot", page_icon="🤖", layout="centered")
 st.title("🤖 RAG Chatbot")
@@ -21,6 +25,9 @@ if on:
 if st.sidebar.button('CLEAR 🗑'):
     st.session_state.chat_history.clear()
 
+if st.button("Add Docs"):
+    st.switch_page("pages/docloaderui.py") 
+
 # --- Display Previous Messages ---
 for msg in st.session_state.chat_history:
     if isinstance(msg, HumanMessage):
@@ -32,7 +39,19 @@ for msg in st.session_state.chat_history:
 
 parser = StrOutputParser() # Ensure outputs are strings
 
-user_input = st.chat_input("Type your message here...")
+if st.sidebar.button('rec'):
+    user_input = st.chat_input("Type your message here...")
+    with st.spinner("Speak something..."):
+        with sr.Microphone() as source:
+            audio = recognizer.listen(source,phrase_time_limit=5)
+
+        try:
+            text = recognizer.recognize_google(audio)
+            user_input=text
+        except sr.UnknownValueError:
+            st.error("❌ Could not understand audio")
+else:
+    user_input = st.chat_input("Type your message here...")
 
 if user_input:
     st.session_state.chat_history.append(HumanMessage(content=user_input))
